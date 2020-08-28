dcacdea7b7c782c42e7e41bcb0c181e04fb47b27
cadon0 <caleb.donnelly@lic.co.nz>
2020-08-28 23:38:31 +1200

    Get BPM on request, replace separate polling script
    Move config into one file

---
+++ src/scripts/getBpmFromFile.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+
+/**
+ * Checks specified file for a new BPM value
+ * @returns {string} the BPM the gif should play at
+ */
+getBpm = (currentBpm, config) => {
+  const newBpm = fs.readFileSync(path.resolve(config.bpmFile), "utf-8");
+  if (currentBpm != newBpm) console.log(`Changing BPM to ${newBpm}`);
+  return newBpm;
+};
+
+module.exports = { getBpm };
