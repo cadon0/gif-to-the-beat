03c9d79d28cc6c8428b276c01a74d353cb12cec4
cadon0 <calebdonnellynz@gmail.com>
2020-09-17 20:56:49 +1200

    Add script to run osu! memory reader .exe

---
+++ server/runOsuMemoryReader.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const { execFile } = require("child_process");
+const { osuDataFeedPort } = require("./config");
+
+runOsuMemoryReader = () => {
+  const exe = path.resolve(__dirname, "../GifToTheBeat.exe");
+  if (fs.existsSync(exe)) {
+    console.log("Starting osu! memory reader...");
+    execFile(exe, [osuDataFeedPort], (error, stdout, stderr) => {
+      if (error) {
+        console.error("Error running osu! memory reader:");
+        console.error();
+        runOsuMemoryReader();
+      }
+      console.log(stdout);
+      console.error(stderr);
+    });
+  } else {
+    console.error(`File not found: ${exe}`);
+    console.error(
+      "This is downloaded separately, please refer to the instructions."
+    );
+    process.exit(1);
+  }
+};
+
+module.exports = { runOsuMemoryReader };
